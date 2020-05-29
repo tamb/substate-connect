48e9e01e91157997350327e353a5719d41814145
Tom Saporito <tamb89@protonmail.com>
2020-05-28 22:54:09 -0400

    WIP v4 create Provide HOC

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
 const path = require('path');
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname),
-        filename: 'index.js',     
-        library: 'substateConnect',
-        libraryTarget: 'umd'
+
+
+const reactConfig = {
+  entry: './src/react/index.js',
+  output: {
+      path: path.resolve(__dirname),
+      filename: 'dist/react/index.js',     
+      library: 'substateConnect',
+      libraryTarget: 'umd'
+  },
+  module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
     },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"]
-          }
-        ]
-      },
-}
+};
+
+
+const backwardsCompatConfig = {
+  entry: './src/react/index.js',
+  output: {
+      path: path.resolve(__dirname),
+      filename: 'dist/index.js',     
+      library: 'substateConnect',
+      libraryTarget: 'umd'
+  },
+  module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+};
+
+
+module.exports = [reactConfig, backwardsCompatConfig];
